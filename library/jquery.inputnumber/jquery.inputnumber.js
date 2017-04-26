e52919fd47c1397b6662e76143e262a372f60868
Frédéric BISSON <zigazou@free.fr>
2017-04-26 23:25:49 +0200

    Use BootStrap 4 Use FontAwesome

--- library/jquery.inputnumber/jquery.inputnumber.js
+++ library/jquery.inputnumber/jquery.inputnumber.js
@@ -1,17 +1,21 @@
 $('input[type=number]').each(function(index, element) {
-    var bPlus = $('<button class="glyphicon glyphicon-plus btn btn-info"><span class="hidden">+</span></button>')
-        bMinus = $('<button class="glyphicon glyphicon-minus btn btn-info"><span class="hidden">-</span></button>');
+    "use strict";
+    var bPlus = $(
+        '<button class="fa fa-plus btn btn-info">'
+        + '<span class="btn-hidden">+</span>'
+        + '</button>'
+    );
+
+    var bMinus = $(
+        '<button class="fa fa-minus btn btn-info">'
+        + '<span class="btn-hidden">-</span>'
+        + '</button>'
+    );
 
     bPlus.insertAfter(element);
     bMinus.insertAfter(element);
 
-    bPlus.on('click', function(event) {
-        element.stepUp();
-        return false;
-    });
-    bMinus.on('click', function(event) {
-        element.stepDown();
-        return false;
-    });
+    bPlus.on('click', function(event) { element.stepUp(); return false; });
+    bMinus.on('click', function(event) { element.stepDown(); return false; });
 });
 
