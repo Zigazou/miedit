8c468fb4b010b09dacbb614157ca5f2ac9788a2e
Frédéric BISSON <zigazou@free.fr>
2017-08-08 21:03:05 +0200

    Use of websocket on Minitel keyboard

--- app/minitel-keyboard.js
+++ app/minitel-keyboard.js
@@ -3,19 +3,32 @@ window.addEventListener("load", function(event) {
     const canvas = document.getElementById("minitel-screen")
     const screen = new MinitelScreen(canvas)
 
-    const standardKey = new KeySimulator(
-        document.getElementById("minitel-standardkey-down"),
-        document.getElementById("minitel-standardkey-up")
-    )
+    const socket = new WebSocket("ws://minitel.ouep.eu/ws/")
 
-    document.addEventListener("keydown", event => {
-        standardKey.pressKey(event.key)
-        event.preventDefault()
-        screen.send([event.key.charCodeAt(0)])
-    })
+    socket.onopen = event => {
+        const standardKey = new KeySimulator(
+            document.getElementById("minitel-standardkey-down"),
+            document.getElementById("minitel-standardkey-up")
+        )
 
-    document.addEventListener("keyup", event => {
-        standardKey.releaseKey(event.key)
-        event.preventDefault()
-    })
+        document.addEventListener("keydown", event => {
+            standardKey.pressKey(event.key)
+            event.preventDefault()
+            socket.send(event.key)
+        })
+
+        document.addEventListener("keyup", event => {
+            standardKey.releaseKey(event.key)
+            event.preventDefault()
+        })
+
+        socket.onmessage = function(event) {
+            const message = []
+            range(event.data.length).forEach(offset => {
+                message.push(event.data[offset].charCodeAt(0))
+            })
+
+            screen.send(message)
+        }
+    }
 })
