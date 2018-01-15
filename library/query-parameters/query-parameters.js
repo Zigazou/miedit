17d7d511680afddddfb09b57a54899830e2e8d8a
Frédéric BISSON <zigazou@free.fr>
2018-01-15 18:31:25 +0100

    Added documentation for queryParameters

--- library/query-parameters/query-parameters.js
+++ library/query-parameters/query-parameters.js
@@ -1,4 +1,9 @@
 "use strict"
+/**
+ * Return a specific parameter value from the current URL
+ * @param {string} varName Name of the parameter from which value will be read
+ * @return {?string} The value or undefined if not available
+ */
 function queryParameters(varName) {
     function decode(str) { return decodeURIComponent(str.replace('+', ' ')) }
 
