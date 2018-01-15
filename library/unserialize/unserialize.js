0168d4860457fd126767a80a8b991164627ed2f3
Frédéric BISSON <zigazou@free.fr>
2018-01-15 18:27:49 +0100

    Added documentation for unserialize

--- library/unserialize/unserialize.js
+++ library/unserialize/unserialize.js
@@ -1,20 +1,37 @@
-// Unserialize previously serialized form values into a form
+/**
+ * This function helps to extract and restore a form state by unserializing
+ * previously serialized values. The serialized string is of the form
+ *
+ *     key=value&key=value&key=value...
+ *
+ * 
+ *
+ * @param {string} serialized The serialized values
+ */
 Element.prototype.unserialize = function(serialized) {
     if(serialized === undefined || serialized === "") return
 
+    // Split the string on the ampersand
     serialized.split("&").map(elem => {
+        // Split the string on the equal sign
         const pairs = elem.split("=")
+
+        // Decode the value
         const value = decodeURIComponent(pairs[1].replace(/\+/g, "%20"))
 
+        // Look for an element with the given name
         for(let node of this.querySelectorAll("[name=" + pairs[0] + "]")) {
             switch(node.getAttribute("type")) {
                 case "checkbox":
+                    // Checkboxes use the checked attribute
                     node.checked = true
                     break
                 case "radio":
+                    // Radio boxes are a group of element
                     if(value == node.value) node.checked = true
                     break
                 default:
+                    // Any other type of input element
                     node.value = value
             }
         }
