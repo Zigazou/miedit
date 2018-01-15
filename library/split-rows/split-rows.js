ec060c0323264a273a253eba1389e1bc6ae0ff30
Frédéric BISSON <zigazou@free.fr>
2018-01-15 18:17:40 +0100

    Added documentation for splitRows function

--- library/split-rows/split-rows.js
+++ library/split-rows/split-rows.js
@@ -1,12 +1,26 @@
 "use strict"
 
+/**
+ * Take a string and slice it into strings of specified length. Multiple spaces
+ * are merged into one space. This function keeps line returns. Line returns
+ * must follow the Unix convention.
+ *
+ * @param {string} str The string to slice
+ * @param {number} width The maximum length of a resulting string
+ * @return {string[]}
+ */
 function splitRows(str, width) {
+    // Merge spaces
     str = str.replace(/ +/g, " ")
 
     const rows = []
+    // The string may contain many rows
     for(let bit of str.split("\n")) {
         let row = ""
+
+        // The row may contain many words
         for(let word of bit.split(" ")) {
+            // Verify the next word won't make the string exceed its max length
             if(row.length + 1 + word.length > width) {
                 rows.push(row)
                 row = ""
